4c120260d8199ca934fb5c0e7d81884a6a8c393d
chanchal16 <rajputchanchal17@gmail.com>
2025-04-15 15:19:17 +0530

    update: add editor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
+import "./App.css";
+import Editor from "./components/Editor";
 
 function App() {
+  const note = {
+    id: "001",
+    folderId: "123",
+    title: "first note",
+    content: "this is sample note", // stored as Tiptap JSON
+    updatedAt: 2,
+  };
 
   return (
-    <div>
-      <h1 className='text-2xl text-red-500'>hey there</h1>
+    <div className="w-3/4 h-screen mx-auto p-4 bg-slate-200">
+      <Editor note={note} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
