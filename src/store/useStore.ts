68d06f8f439bc3af207ab52c9daeb7c0bf947efc
chanchal16 <rajputchanchal17@gmail.com>
2025-04-16 11:04:36 +0530

    update: implemented addnote and updateNote function

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -7,9 +7,15 @@ export const useStore = create<Store>()(
     (set, get) => ({
       folders: [],
       notes: [],
+      selectedNoteId: null,
+      setSelectedNoteId: (noteId) => set({ selectedNoteId: noteId }),
       addFolder: () => {},
-      addNote: () => {},
-      updateNote: () => {},
+      addNote: (note) =>
+        set((prevState) => ({ notes: [...prevState.notes, note] })),
+      updateNote: (note) =>
+        set((state) => ({
+          notes: state.notes.map((n) => (n.id === note.id ? note : n)),
+        })),
       deleteNote: () => {},
     }),
     { name: "notes-app-store" }
