98e0b08499d7f053e4eda63c6629c35c8b024ef3
chanchal16 <rajputchanchal17@gmail.com>
2025-05-03 18:33:20 +0530

    feat: create foldersPopover component

---
+++ src/components/FoldersPopover.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Folder, FolderInput } from "lucide-react";
+import { useStore } from "@/store/useStore";
+import { searchFolders } from "@/lib/searchFolders";
+import { Note } from "@/types/type";
+
+const FoldersPopover = ({ note }: { note: Note }) => {
+  const { folders, selectedFolderId, moveNoteToFolder, setOpenDialog } =
+    useStore();
+  const [searchFolderQuery, setSearchFolderQuery] = useState<string>("");
+
+  const filteredFolders = searchFolders(
+    folders,
+    searchFolderQuery.toLowerCase()
+  );
+
+  const handleMoveNote = (noteId: string, folderId: string) => {
+    moveNoteToFolder(noteId, folderId);
+    setSearchFolderQuery("");
+    setOpenDialog(false);
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="border-none">
+          <FolderInput size={16} />
+        </button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-64 md:w-80 max-h-60 overflow-auto flex flex-col gap-2">
+        <input
+          type="text"
+          className="outline-none border border-gray-300 p-1"
+          placeholder="search folder"
+          value={searchFolderQuery}
+          onChange={(e) => setSearchFolderQuery(e.target.value)}
+        />
+        {filteredFolders.map((folder) => (
+          <div
+            key={folder.id}
+            onClick={() => handleMoveNote(note.id, folder.id)}
+            className={`flex gap-2 p-1 items-center hover:bg-gray-100 cursor-pointer ${
+              selectedFolderId === folder.id ? "bg-gray-100" : "bg-transparent"
+            }`}
+          >
+            <Folder style={{ color: folder.color }} size={18} />
+            <span>{folder.name}</span>
+          </div>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default FoldersPopover;
