1318ca14728bdae70fb32487eb586c8618629325
chanchal16 <rajputchanchal17@gmail.com>
2025-05-05 12:04:51 +0530

    feat: create NoteColorPicker component

---
+++ src/components/NoteColorPicker.tsx
@@ -0,0 +1,40 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { noteColors } from "@/lib/colors";
+import { Note } from "@/types/type";
+import { Palette } from "lucide-react";
+
+const NoteColorPicker = ({
+  note,
+  onChangeColor,
+}: {
+  note: Note;
+  onChangeColor: (color: string) => void;
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <button className="p-2 hover:bg-[#5f636826] hover:rounded-full">
+          <Palette size={18} />
+        </button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto flex flex-wrap gap-2 p-2">
+        {noteColors.map((color: string) => (
+          <button
+            key={color}
+            onClick={() => onChangeColor(color)}
+            className={`w-6 h-6 rounded-full border ${
+              note.color === color ? "border-black" : "border-transparent"
+            }`}
+            style={{ backgroundColor: color }}
+          />
+        ))}
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default NoteColorPicker;
