2b091b49f1a5604aed2ebccde62c6c98b1523af3
chanchal16 <rajputchanchal17@gmail.com>
2025-05-06 21:52:04 +0530

    update: add toggle state

--- src/components/NoteColorPicker.tsx
+++ src/components/NoteColorPicker.tsx
@@ -6,28 +6,30 @@ import {
 import { noteColors } from "@/lib/colors";
 import { Note } from "@/types/type";
 import { Palette } from "lucide-react";
+import { useState } from "react";
 
 const NoteColorPicker = ({
   note,
   onChangeColor,
 }: {
-  note: Note;
+  note?: Note;
   onChangeColor: (color: string) => void;
 }) => {
+  const [showPopover, setShowPopover] = useState<boolean>(false);
   return (
-    <Popover>
+    <Popover open={showPopover} onOpenChange={setShowPopover}>
       <PopoverTrigger asChild>
         <button className="p-2 hover:bg-[#5f636826] hover:rounded-full">
           <Palette size={18} />
         </button>
       </PopoverTrigger>
       <PopoverContent className="w-auto flex flex-wrap gap-2 p-2">
-        {noteColors.map((color: string) => (
+        {noteColors?.map((color: string) => (
           <button
             key={color}
             onClick={() => onChangeColor(color)}
             className={`w-6 h-6 rounded-full border ${
-              note.color === color ? "border-black" : "border-transparent"
+              note?.color === color ? "border-black" : "border-transparent"
             }`}
             style={{ backgroundColor: color }}
           />
