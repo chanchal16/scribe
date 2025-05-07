9d81a9bfef96c1920ae1600210efb60d58687e9d
chanchal16 <rajputchanchal17@gmail.com>
2025-05-07 17:59:21 +0530

    refactor: remove commented code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,19 +9,14 @@ const Navbar = ({ onToggleSidebar }: Props) => {
   const { searchQuery, setSearchQuery } = useStore();
 
   return (
-    <div className="p-2 w-full flex items-center gap-4">
-      {/* Hamburger */}
+    <div className=" py-3 border-b w-full flex items-center gap-4">
       <button
         onClick={onToggleSidebar}
-        className="md:hidden p-2 rounded hover:bg-gray-100"
+        className="md:hidden p-2 rounded text-gray-500 hover:bg-gray-100"
       >
-        <Menu size={24} />
+        <Menu size={20} />
       </button>
-
-      {/* Logo */}
       <h3 className="text-2xl font-kalam font-bold text-amber-400">Scribe</h3>
-
-      {/* Search */}
       <input
         type="text"
         placeholder="search..."
